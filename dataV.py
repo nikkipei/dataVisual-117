a72dddfd443803f41ef16864f0a177cef088dbb0
nikkipei <peidongyu1029@gmail.com>
2021-08-25 19:50:24 -0400

    qtweb
    worked mapbox public

--- dataV.py
+++ dataV.py
@@ -10,8 +10,9 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow,self).__init__(*args, **kwargs)
 
+        
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://www.google.com"))
+        self.browser.setUrl(QUrl("https://api.mapbox.com/styles/v1/nikkipei/cksqn08nj1dk417mrb3ryqx5p.html?fresh=true&title=view&access_token=pk.eyJ1Ijoibmlra2lwZWkiLCJhIjoiY2tzcDJqMm85MDFvNDJ3cDc0MXhkeWFhaCJ9.vK5iDtMm7eZ5q6Gwtt1UDw"))
 
         self.setCentralWidget(self.browser)
 
